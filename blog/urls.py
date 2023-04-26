0a86ed47764abda2b32e57bb0bc04665a86e73cb
rafaelgavio <rafaelcgaviolli@outlook.com>
2023-04-25 21:22:00 -0300

    creating comments in posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from blog import views
 
 urlpatterns = [
     path("", views.PostView.as_view(), name='home'),
-    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+    # path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+    path("<slug:slug>/", views.post_detail, name="post_detail"),
 ]
